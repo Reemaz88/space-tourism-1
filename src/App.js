c3d715248a90b24dc18a08f67bc7d75c9fb04858
reemaz al soufe <remoso88@gmail.com>
2024-09-12 08:33:50 +0400

    last

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Crew from './components/Crew';
@@ -7,14 +7,14 @@ import Technology from './components/Technology';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/destination" element={ <Destination /> } />
-          <Route path="/technology" element={ <Technology />} />
-          <Route path="/crew" element={ <Crew /> } />
-          <Route exact path="/" element={ <Home /> } />
+          <Route path='/destination' element={<Destination />} />
+          <Route path='/technology' element={<Technology />} />
+          <Route path='/crew' element={<Crew />} />
+          <Route exact path='*' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
